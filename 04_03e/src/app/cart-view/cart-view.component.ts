b4db799820f3fbd5cd28985874d565daf104a1ed
alcfeoh <alain@interstate21.com>
2025-03-12 15:17:11 -0700

    Video 4-03

---
+++ 04_03e/src/app/cart-view/cart-view.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {LicensePlate} from '../license-plate';
+import {CartService} from '../cart.service';
+import {NgIf} from '@angular/common';
+import {JumbotronComponent} from '../jumbotron/jumbotron.component';
+
+@Component({
+  selector: 'app-cart-view',
+  standalone: true,
+  imports: [ NgIf, JumbotronComponent],
+  templateUrl: './cart-view.component.html',
+  styleUrls: ['./cart-view.component.css']
+})
+export class CartViewComponent {
+
+  cartContents: LicensePlate[] = [];
+
+  constructor(private service: CartService) {
+    service.getCartContents().subscribe(data => this.cartContents = data);
+  }
+
+  removeFromCart(plate: LicensePlate): void {
+    // TODO
+  }
+
+}
