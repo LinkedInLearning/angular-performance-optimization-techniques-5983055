eced6824796a4c6616aa2e93b1b415e9f311bb66
alcfeoh <alain@interstate21.com>
2025-03-12 16:05:08 -0700

    Video 4-05

---
+++ 04_05b/src/app/store-view/store-view.component.ts
@@ -0,0 +1,19 @@
+import {Component, inject} from '@angular/core';
+import {JumbotronComponent} from '../jumbotron/jumbotron.component';
+import {HttpClient} from '@angular/common/http';
+import {LicensePlate} from '../license-plate';
+import {AsyncPipe, SlicePipe} from '@angular/common';
+import {LicensePlateComponent} from '../license-plate/license-plate.component';
+
+@Component({
+    selector: 'app-store-view',
+  imports: [JumbotronComponent, AsyncPipe, LicensePlateComponent, SlicePipe],
+    templateUrl: './store-view.component.html',
+    styleUrl: './store-view.component.css'
+})
+export class StoreViewComponent {
+
+  http = inject(HttpClient);
+  plates$ = this.http.get<LicensePlate[]>("https://lp-store-server.vercel.app/data");
+
+}
