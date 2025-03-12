03e94df644ba207313a7c70aed5552ba0ab3f443
alcfeoh <alain@interstate21.com>
2025-03-12 15:05:36 -0700

    Video 4-02

---
+++ 04_02b/src/app/navigation/navigation.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {CurrencySwitcherComponent} from '../currency-switcher/currency-switcher.component';
+
+@Component({
+    selector: 'app-navigation',
+    imports: [
+        RouterLink,
+        CurrencySwitcherComponent
+    ],
+    templateUrl: './navigation.component.html',
+    styleUrl: './navigation.component.css'
+})
+export class NavigationComponent {
+
+}
